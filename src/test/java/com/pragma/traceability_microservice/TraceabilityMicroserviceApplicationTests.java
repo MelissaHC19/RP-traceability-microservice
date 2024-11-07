5855e8a1e91f3c85b445c839e917fe8b426d443a
Melissa Henao Cardona <melissa.henao@utp.edu.co>
2024-11-06 23:43:42 -0500

    project creation

---
+++ src/test/java/com/pragma/traceability_microservice/TraceabilityMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.traceability_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraceabilityMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
